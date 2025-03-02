1cfafbc78b4b6a98f4518b9a61bb7c919a3e97d5
Иван Сафронов <issafronov@hubble.local>
2025-03-02 15:30:04 +0300

    chi

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
 	"github.com/issafronov/shortener/internal/app/handlers"
 	"net/http"
 )
@@ -11,8 +12,13 @@ func main() {
 	}
 }
 
+func Router() chi.Router {
+	router := chi.NewRouter()
+	router.Get("/{key}", handlers.MainPage)
+	router.Post("/", handlers.MainPage)
+	return router
+}
+
 func runServer() error {
-	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, handlers.MainPage)
-	return http.ListenAndServe(`localhost:8080`, mux)
+	return http.ListenAndServe(`localhost:8080`, Router())
 }
---
+++ cmd/shortener/main_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"github.com/issafronov/shortener/internal/app/handlers"
+	"github.com/issafronov/shortener/internal/app/storage"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/go-resty/resty/v2"
+)
+
+func init() {
+	storage.Urls = map[string]string{}
+}
+
+func TestMainPage(t *testing.T) {
+	handler := http.HandlerFunc(handlers.MainPage)
+	srv := httptest.NewServer(handler)
+	defer srv.Close()
+
+	type want struct {
+		code        int
+		contentType string
+	}
+
+	var key string
+
+	tests := []struct {
+		name   string
+		method string
+		want   want
+	}{
+		{
+			name:   "Generate link",
+			method: http.MethodPost,
+			want: want{
+				code:        201,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name:   "Go to link",
+			method: http.MethodGet,
+			want: want{
+				code:        307,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req := resty.New().R()
+			req.Method = test.method
+			if test.method == http.MethodPost {
+				req.Body = strings.NewReader("https://www.google.com")
+			}
+
+			req.URL = srv.URL
+
+			if test.method == http.MethodGet {
+				req.URL = key
+			}
+
+			fmt.Println(req.URL)
+			res, err := req.Send()
+			require.NoError(t, err, "error making HTTP request")
+			assert.Equal(t, test.want.code, res.StatusCode())
+			key = string(res.Body())
+			fmt.Println("after test ", key)
+			assert.Equal(t, test.want.contentType, res.Header().Get("Content-Type"))
+		})
+	}
+}
